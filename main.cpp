30b8f54e0f705bab7e506ed7bfb5b7bb3f7b0a21
Kevin Veld <kevin.veld@student.hu.nl>
2019-01-31 12:53:16 +0100

    with template

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@
 #include <string>
 #include <ctime>
 
-std::vector<std::string> generateStrings(std::vector<std::string> possibilities, int size){
-  std::vector<std::string> stringList;
+template <typename T>
+std::vector<T> genStrings(std::vector<T> possibilities, int size){
+  std::vector<T> items;
   for(int i = 0; i < size; ++i)
-    stringList.push_back(possibilities[std::rand() % possibilities.size()]);
-  return stringList;
+    items.push_back(possibilities[std::rand() % possibilities.size()]);
+  return items;
 }
-
 int countItem(std::vector<std::string> itemList, std::string item){
   int nItem = 0;
   for(std::string i : itemList)
       if(i == item) nItem++;
   return nItem;
 }
-
 int main(){
   srand(time(NULL));
-  std::vector<std::string> coinList = generateStrings({"heads", "tails"}, 100);
+  std::vector<std::string> coinPossibilities = {"heads", "tails"};
+  std::vector<std::string> coinList = genStrings(coinPossibilities, 100);
   int nTails = countItem(coinList, "tails"),
       nHeads = countItem(coinList, "heads");
 
